42bc01c29ed0e113198e195a92498d3d80769791
Prince Leo <princeleo400.ohagamichael@gmail.com>
2023-01-08 00:29:39 +0100

    added cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,9 @@
 const allowedOrigins = [
     'https://good-tan-viper-hose.cyclic.app',
+    'https://rubyslibrary.netlify.app',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
+    'http://localhost:3001',
     'http://localhost:8000'
 ];
 
